afbf0c2ff5b0522dd5fc11ee2337bd1fee430f30
swasthi15@gmail.com <swasthi15@gmail.com>
2019-12-02 22:46:20 +0530

    final

--- chat/views.py
+++ chat/views.py
@@ -6,7 +6,7 @@ def index(request):
     return render(request, 'index2.html', {})
 
 def room(request, room_name):
-    return render(request, 'room.html', {
+    return render(request, 'room1.html', {
         'room_name_json': mark_safe(json.dumps(room_name)),
         'username':request.user
     })
